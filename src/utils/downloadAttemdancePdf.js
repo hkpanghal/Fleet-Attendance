92a19b60463f1823a0b07fa81e492cd01e78338d
hkpanghal <112403425+hkpanghal@users.noreply.github.com>
2024-03-28 21:56:18 +0530

    fixed pdfmake issues

--- src/utils/downloadAttemdancePdf.js
+++ src/utils/downloadAttemdancePdf.js
@@ -1,4 +1,4 @@
-import pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
